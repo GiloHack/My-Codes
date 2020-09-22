81914085780f9199e8a866f97c4229f99a599809
GiloHack <68976168+GiloHack@users.noreply.github.com>
2020-09-22 16:45:22 +0300

    Create Register.php
    A Registration page that saves data to the database

---
+++ Register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+<title>Register | Example</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="author" content="Gilbert Oloya"/>
+<meta name="keyword" content="Login Sample template"/>
+</head>
+<body>
+<div class="form-inputs">
+<form method="POST">
+<h2>LRegister Here</h2>
+<p>If you have an Account, <a href="">Login</a> here</p>
+
+</form>
+</div>
+
+</body>
+</html>
