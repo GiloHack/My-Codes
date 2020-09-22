6a2a283fd4b78b76056afa24752268870e39b1d7
GiloHack <68976168+GiloHack@users.noreply.github.com>
2020-09-22 17:00:12 +0300

    Update Register.php

--- Register.php
+++ Register.php
@@ -11,7 +11,25 @@
 <form method="POST">
 <h2>LRegister Here</h2>
 <p>If you have an Account, <a href="">Login</a> here</p>
-
+<form method="POST" action="Register.php" enctype="multipart/form-data"/>
+  <label>Email</label>
+    <input type="email"  placeholder="Email address" required name="email"  class=""/>
+  <label>First name</label>   
+  <input type="text"  placeholder="First name" required name="fname"  class=""/>
+  
+  <label>Last name</label>
+  <input type="text"  placeholder="Lastname" required name="lname"  class=""/>
+  <label>Gender</label> 
+  <select  name="gender" required>
+     <option value="Male">Male</option>
+     <option value="Female">Female</option>
+     <option value="Other">Other</option>  
+  </select>
+  <label>Date of birth</label>
+  <input type="date" name="dob"required/>
+  <input type="submit" value="Register" name="reg"/>
+  
+  
 </form>
 </div>
 
